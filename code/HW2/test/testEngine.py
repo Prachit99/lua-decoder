0226b587c34a6584e86ba2f9deeeb3eb623a7a29
Prachit Mhalgi <prachitmhalgi@gmail.com>
2023-01-30 00:38:56 -0500

    fixed test cases

--- code/HW2/test/testEngine.py
+++ code/HW2/test/testEngine.py
@@ -1,23 +1,29 @@
 import sys
 sys.path.insert(1, "../")
 import Main
+import Constants
 from testNum import TestNum
 from testSym import TestSym
-from testThe import TestThe
+# from testThe import TestThe
 from testRand import TestRand
 
 
 egs = {}
 
 def eg(key, s, fun):
-    global help
+    const = Constants.Constants()
+    help = const.help
     egs[key] = fun
     help += "  -g  {}\t{}\n".format(key, s)
 
 
-eg("the", "show settings", TestThe.testThe)
-eg("rand", "generate, reset, regenerate same", TestRand.testRand)
+# eg("the", "show settings", TestThe.testThe)
+eg("rand", "check random", TestRand.testRand)
 eg("sym", "check syms", TestSym.testSym)
 eg("num", "check nums", TestNum.testNum)
 # eg("csv","read from csv", check_csv)
-Main.main(egs)
+
+const = Constants.Constants()
+help = const.help
+options = dict()
+Main.main(options, help, egs)
