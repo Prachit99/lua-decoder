a3b26d245c44da26401c5cd5417ae96020907b01
neha7799 <np771999@gmail.com>
2023-02-04 12:20:27 -0500

    fixed data.py

--- code/HW3/Data.py
+++ code/HW3/Data.py
@@ -7,51 +7,57 @@ from Utils import many
 from Utils import any
 from Utils import push
 from Utils import lt
+import Utils 
 from Row import Row
 from Cols import Cols
 import math
 import The
 
 class Data:
-    def __init__(self,src,fun):
+    def __init__(self,src=None):
         self.rows = []
         self.cols = None
         fun=lambda x: self.add(x)
         if type(src)==str:
-            csv(src,fun)
+            self.csv(src,fun)
         else:
-            map(src or [], fun)
+            #DOUBT FUNC CALL
+            if(src!=None):
+                Utils.map(src,fun)
+            else:
+                Utils.map([],fun)
     
 
     def add(self,t):
         if(len(self.cols)!=0):
-            if len(t.cells)==0:
-                t=Row(t)
+            t=Row(t)
             self.rows.append(t)
             self.cols.add(t)
         else:
             self.cols=Cols(t)
 
 
-    
-    def clone(self,init,data):
+    def clone(self,init=None):
         data=Data(list(self.cols.names))
         fun=lambda x: data.add(x)
-        map(init or [] ,fun)
+        if init:
+            Utils.map(init,fun)
+        else:
+            Utils.map([],fun)
         return data
 
-
-    def stats(self,what,cols,nPlaces):
+#DOUBT
+    def stats(self,what,cols,nPlaces,fun):
         def fun(k,col):
             return col.rnd((col,what)(), nPlaces), col.txt
         return kap(cols or self.cols.y, fun)
     
 
-    def better(self,row1,row2,s1,s2,ys,x,y):
+    def better(self,row1,row2):
         s1=0
         s2=0
         ys=self.cols.y
-        for idx,col in enumerate(ys):
+        for col in ys:
             x=col.norm(row1.cells[col.at])
             y=col.norm(row2.cells[col.at])
             s1=s1-(pow(math.e,(col.w*(x-y))/len(ys)))
@@ -59,36 +65,42 @@ class Data:
         return s1/len(ys) < s2/len(ys)
     
 
-    def dist(self,row1,row2,cols,n,d):
+    def dist(self,row1,row2,cols=None):
         n=0
         d=0
-#?????????? not sure about 'or' in for loop
-        for col in cols or self.cols.x:
+        if cols:
+            li=cols
+        else:
+            li=self.cols.x
+        for col in li:
             n+=1
             d+=pow(col.dist(row1.cells[col.at],row2.cells[col.at]),The.p)
         return pow((d/n),(1/The.p))
     
-#AROUND FUNCTION IS MESSY, MOSTLY WRONG????/HELP
-    def around(self,row1,rows,cols):
-        return sort(map(rows or self.rows, self.around_helper(row1,row2,cols)),lt("dist"))
-    
 
-    def around_helper(self,row1,row2,cols):
-        return {row2:self.dist(row1,row2,cols)}
+    def around(self,row1,rows=None,cols=None):
+        if rows!=None:
+            li=rows
+        else:
+            li=self.rows
+        around_li=[]
+        for r in li:
+            around_li.append((r, self.dist(row1, r, cols)))
+            around_li.sort(key = lambda x:x[1])
+        return around_li
 
 
-    def half(self,rows,cols,above):
-        some=many(rows,The.Sample)
-        A=above or any(some)
-        #CANT FIND // OPERATOR LUA. ASSUMING INT DIVISION
+    def half(self,rows=None,cols=None,above=None):
+        some=Utils.many(rows,The.Sample)
+        A=above if above != None else Utils.any(some)
         B=self.around(A,some)[The.Far*len(rows)//1].row
         c=self.dist(A,B,cols)
-        def project(row):
-        #dictionary doesnt have key's name like lua.just key:value pairs???
+        def project(row,A,B,c,cols):
             return {row,cosine(self.dist(row,A,cols),self.dist(row,B,cols),c)}
+        rows=rows if rows!=None else self.rows
         left=[]
         right=[]
-        for n,tmp in enumerate(sort(map(rows,project),lt("dist"))):
+        for n,tmp in enumerate(sort(Utils.map(rows,project),lt("dist"))):
             if n<=(len(rows)//2):
                 push(left,tmp.row)
                 mid=tmp.row
@@ -97,34 +109,28 @@ class Data:
         return left,right,A,B,mid,c
     
 
-#MANY PYTHON KEYWORDS USED. HAVE TO HANDLE
-    def cluster(self,rows,min,cols,above):
-        #left and right initialized as empty lists? correct?
-        left=[]
-        right=[]
-        rows=rows or self.rows
-        min=min or pow(len(rows),The.min)
-        cols=cols or self.cols.x
-        node=[self.clone(rows)]
-        if len(rows)>2*min:
-            left,right,node.A,node.B,node.mid=self.half(rows,cols,above)
-            node.left=self.cluster(left,min,cols,node.A)
-            node.right=self.cluster(right,min,cols,node.B)
+    def cluster(self,rows=None,minn=None,cols=None,above=None):
+        rows=rows if rows!=None else self.rows
+        minn=minn if minn!=None else pow(len(rows),The.minn)
+        cols=cols if cols!=None else self.cols.x
+        node={"data":self.clone(rows)}
+        if len(rows)>2*minn:
+            left,right,node["A"],node["B"],node["mid"]=self.half(rows,cols,above)
+            node.left=self.cluster(left,minn,cols,node["A"])
+            node.right=self.cluster(right,minn,cols,node["B"])
         return node
     
 
-    def sway(self,rows,min,cols,above):
-        left=[]
-        right=[]
-        rows=rows or self.rows
-        min=min or pow(len(rows),The.min)
-        cols=cols or self.cols.x
-        node=[self.clone(rows)]
-        if len(rows)>2*min:
-            left,right,node.A,node.B,node.mid=self.half(rows,cols,above)
-            if self.better(node.B,node.A):
-                left,right,node.A,node.B=right,left,node.B,node.A
-            node.left=self.sway(left,min,cols,node.A)
+    def sway(self,rows=None,minn=None,cols=None,above=None):
+        rows=rows if rows!=None else self.rows
+        minn=minn if minn!=None else pow(len(rows),The.minn)
+        cols=cols if cols!=None else self.cols.x
+        node={"data":self.clone(rows)}
+        if len(rows)>2*minn:
+            left,right,node["A"],node["B"],node["mid"],node["c"]=self.half(rows,cols,above)
+            if self.better(node["B"],node["A"]):
+                left,right,node["A"],node["B"]=right,left,node["B"],node["A"]
+            node.left=self.sway(left,minn,cols,node["A"])
         return node
 
 
