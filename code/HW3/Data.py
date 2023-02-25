9679c17f1241dc2e39a7aabdd7af4b3c98d8cf29
neha7799 <np771999@gmail.com>
2023-02-24 22:02:34 -0500

    updated data.py

--- code/HW3/Data.py
+++ code/HW3/Data.py
@@ -57,73 +57,73 @@ class Data:
         return s1/len(ys) < s2/len(ys)
     
 
-    def dist(self,row1,row2,cols=None):
-        n=0
-        d=0
-        if cols:
-            li=cols
-        else:
-            li=self.cols.x
-        for col in li:
-            n+=1
-            d+=pow(col.dist(row1.cells[col.at],row2.cells[col.at]),The.p)
-        return pow((d/n),(1/The.p))
+    # def dist(self,row1,row2,cols=None):
+    #     n=0
+    #     d=0
+    #     if cols:
+    #         li=cols
+    #     else:
+    #         li=self.cols.x
+    #     for col in li:
+    #         n+=1
+    #         d+=pow(col.dist(row1.cells[col.at],row2.cells[col.at]),The.p)
+    #     return pow((d/n),(1/The.p))
     
 
-    def around(self,row1,rows=None,cols=None):
-        if rows!=None:
-            li=rows
-        else:
-            li=self.rows
-        around_li=[]
-        for r in li:
-            around_li.append((r, self.dist(row1, r, cols)))
-            around_li.sort(key = lambda x:x[1])
-        return around_li
+    # def around(self,row1,rows=None,cols=None):
+    #     if rows!=None:
+    #         li=rows
+    #     else:
+    #         li=self.rows
+    #     around_li=[]
+    #     for r in li:
+    #         around_li.append((r, self.dist(row1, r, cols)))
+    #         around_li.sort(key = lambda x:x[1])
+    #     return around_li
 
 
-    def half(self,rows=None,cols=None,above=None):
-        some=Utils.many(rows,The.Sample)
-        A=above if above != None else Utils.any(some)
-        B=self.around(A,some)[The.Far*len(rows)//1].row
-        c=self.dist(A,B,cols)
-        def project(row,A,B,c,cols):
-            return {row,cosine(self.dist(row,A,cols),self.dist(row,B,cols),c)}
-        rows=rows if rows!=None else self.rows
-        left=[]
-        right=[]
-        for n,tmp in enumerate(sort(Utils.map(rows,project),lt("dist"))):
-            if n<=(len(rows)//2):
-                push(left,tmp.row)
-                mid=tmp.row
-            else:
-                push(right,tmp.row)
-        return left,right,A,B,mid,c
+    # def half(self,rows=None,cols=None,above=None):
+    #     some=Utils.many(rows,The.Sample)
+    #     A=above if above != None else Utils.any(some)
+    #     B=self.around(A,some)[The.Far*len(rows)//1].row
+    #     c=self.dist(A,B,cols)
+    #     def project(row,A,B,c,cols):
+    #         return {row,cosine(self.dist(row,A,cols),self.dist(row,B,cols),c)}
+    #     rows=rows if rows!=None else self.rows
+    #     left=[]
+    #     right=[]
+    #     for n,tmp in enumerate(sort(Utils.map(rows,project),lt("dist"))):
+    #         if n<=(len(rows)//2):
+    #             push(left,tmp.row)
+    #             mid=tmp.row
+    #         else:
+    #             push(right,tmp.row)
+    #     return left,right,A,B,mid,c
     
 
-    def cluster(self,rows=None,minn=None,cols=None,above=None):
-        rows=rows if rows!=None else self.rows
-        minn=minn if minn!=None else pow(len(rows),The.minn)
-        cols=cols if cols!=None else self.cols.x
-        node={"data":self.clone(rows)}
-        if len(rows)>2*minn:
-            left,right,node["A"],node["B"],node["mid"]=self.half(rows,cols,above)
-            node["left"]=self.cluster(left,minn,cols,node["A"])
-            node["right"]=self.cluster(right,minn,cols,node["B"])
-        return node
+    # def cluster(self,rows=None,minn=None,cols=None,above=None):
+    #     rows=rows if rows!=None else self.rows
+    #     minn=minn if minn!=None else pow(len(rows),The.minn)
+    #     cols=cols if cols!=None else self.cols.x
+    #     node={"data":self.clone(rows)}
+    #     if len(rows)>2*minn:
+    #         left,right,node["A"],node["B"],node["mid"]=self.half(rows,cols,above)
+    #         node["left"]=self.cluster(left,minn,cols,node["A"])
+    #         node["right"]=self.cluster(right,minn,cols,node["B"])
+    #     return node
     
 
-    def sway(self,rows=None,minn=None,cols=None,above=None):
-        rows=rows if rows!=None else self.rows
-        minn=minn if minn!=None else pow(len(rows),The.minn)
-        cols=cols if cols!=None else self.cols.x
-        node={"data":self.clone(rows)}
-        if len(rows)>2*minn:
-            left,right,node["A"],node["B"],node["mid"],node["c"]=self.half(rows,cols,above)
-            if self.better(node["B"],node["A"]):
-                left,right,node["A"],node["B"]=right,left,node["B"],node["A"]
-            node.left=self.sway(left,minn,cols,node["A"])
-        return node
+    # def sway(self,rows=None,minn=None,cols=None,above=None):
+    #     rows=rows if rows!=None else self.rows
+    #     minn=minn if minn!=None else pow(len(rows),The.minn)
+    #     cols=cols if cols!=None else self.cols.x
+    #     node={"data":self.clone(rows)}
+    #     if len(rows)>2*minn:
+    #         left,right,node["A"],node["B"],node["mid"],node["c"]=self.half(rows,cols,above)
+    #         if self.better(node["B"],node["A"]):
+    #             left,right,node["A"],node["B"]=right,left,node["B"],node["A"]
+    #         node.left=self.sway(left,minn,cols,node["A"])
+    #     return node
 
 
         
